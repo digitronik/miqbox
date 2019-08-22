d1bff820dcafe7b4e14d033bd637cec36f5a8f0f
Nikhil Dhandre <nik.digitronik@live.com>
2019-08-23 00:07:13 +0530

    changes in appliance management
    Signed-off-by: Nikhil Dhandre <nik.digitronik@live.com>

--- miqbox/__init__.py
+++ miqbox/__init__.py
@@ -4,12 +4,18 @@ from miqbox.configuration import config
 from miqbox.images import images
 from miqbox.images import pull
 from miqbox.images import rmi
+from miqbox.miqbox import create
+from miqbox.miqbox import evmserver
+from miqbox.miqbox import kill
+from miqbox.miqbox import start
+from miqbox.miqbox import status
+from miqbox.miqbox import stop
 
 
 @click.version_option()
 @click.group()
 def main():
-    """Spin ManageIQ/CFME Appliance locally with virtualization."""
+    """Spin ManageIQ/CFME Appliance locally with Virtualization."""
     pass
 
 
@@ -18,6 +24,19 @@ main.add_command(images)
 main.add_command(pull)
 main.add_command(rmi)
 
+# MiqBox command
+main.add_command(status)
+
+# Appliance operations commands
+main.add_command(create)
+main.add_command(start)
+main.add_command(stop)
+main.add_command(kill)
+main.add_command(evmserver)
 
 # Configuration command
 main.add_command(config)
+
+
+if __name__ == "__main__":
+    main()
